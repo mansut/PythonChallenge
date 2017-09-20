7ed4a7ff75537a686254fc0e05416e92bb0be9f3
Ale Mansut <mansut@gmail.com>
2017-09-20 11:55:55 +0200

    Commit and push

--- 06_zip.py
+++ 06_zip.py
@@ -1,12 +1,12 @@
 import re
 import zipfile
 
-zipFileName = r"D:\Documenti\A - Studio\Python\pythonchallenge\zip\channel.zip"
-zipObj = zipfile.ZipFile(zipFileName)
-print(zipObj)
-infoList = zipObj.infolist()
-for i in infoList:
-    print(i.comment)
+# zipFileName = r"D:\Documenti\A - Studio\Python\pythonchallenge\zip\channel.zip"
+# zipObj = zipfile.ZipFile(zipFileName)
+# print(zipObj)
+# infoList = zipObj.infolist()
+# for i in infoList:
+#     print(i.comment)
 
 
 # zipDir = r"D:\Documenti\A - Studio\Python\pythonchallenge\zip\\"
@@ -22,3 +22,34 @@ for i in infoList:
 #     print(startingNumer)
 
 
+zipFileName = r"D:\Documenti\A - Studio\Python\pythonchallenge\zip\channel.zip"
+zipObj = zipfile.ZipFile(zipFileName)
+startingNumer = 90052
+
+comments = []
+
+while True:
+    fileName = str(startingNumer) + ".txt"
+    info = zipObj.getinfo(fileName)
+    comments.append(info.comment.decode())
+    line = zipObj.read(fileName).decode()
+    match = re.search("Next nothing is (\d+)",line)
+    if (match == None) : break
+    startingNumer = match.group(1)
+
+print("".join(comments))
+
+# ****************************************************************
+# ****************************************************************
+# **                                                            **
+# **   OO    OO    XX      YYYY    GG    GG  EEEEEE NN      NN  **
+# **   OO    OO  XXXXXX   YYYYYY   GG   GG   EEEEEE  NN    NN   **
+# **   OO    OO XXX  XXX YYY   YY  GG GG     EE       NN  NN    **
+# **   OOOOOOOO XX    XX YY        GGG       EEEEE     NNNN     **
+# **   OOOOOOOO XX    XX YY        GGG       EEEEE      NN      **
+# **   OO    OO XXX  XXX YYY   YY  GG GG     EE         NN      **
+# **   OO    OO  XXXXXX   YYYYYY   GG   GG   EEEEEE     NN      **
+# **   OO    OO    XX      YYYY    GG    GG  EEEEEE     NN      **
+# **                                                            **
+# ****************************************************************
+#  **************************************************************
